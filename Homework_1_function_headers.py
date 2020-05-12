a79b41df95ad55704540dc9d868ee9471e000027
roflmaostc <f.wechsler@tum.de>
2020-05-12 13:39:57 +0200

    Initial commit to add raw functions

---
+++ Homework_1_function_headers.py
@@ -0,0 +1,55 @@
+'''Homework 1, Computational Photonics, SS 2020:  FD mode solver.
+'''
+import numpy as np
+import scipy.sparse as sps
+from scipy.sparse.linalg import eigs
+
+
+def guided_modes_1DTE(prm, k0, h):
+    """Computes the effective permittivity of a TE polarized guided eigenmode.
+    All dimensions are in µm.
+    Note that modes are filtered to match the requirement that
+    their effective permittivity is larger than the substrate (cladding).
+    
+    Parameters
+    ----------
+    prm : 1d-array
+        Dielectric permittivity in the x-direction
+    k0 : float
+        Free space wavenumber
+    h : float
+        Spatial discretization
+    
+    Returns
+    -------
+    eff_eps : 1d-array
+        Effective permittivity vector of calculated modes
+    guided : 2d-array
+        Field distributions of the guided eigenmodes
+    """
+    pass
+
+
+def guided_modes_2D(prm, k0, h, numb):
+    """Computes the effective permittivity of a quasi-TE polarized guided 
+    eigenmode. All dimensions are in µm.
+    
+    Parameters
+    ----------
+    prm  : 2d-array
+        Dielectric permittivity in the xy-plane
+    k0 : float
+        Free space wavenumber
+    h : float
+        Spatial discretization
+    numb : int
+        Number of eigenmodes to be calculated
+    
+    Returns
+    -------
+    eff_eps : 1d-array
+        Effective permittivity vector of calculated eigenmodes
+    guided : 3d-array
+        Field distributions of the guided eigenmodes
+    """
+    pass
---
+++ testscipt1d.py
@@ -0,0 +1,15 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from seminar_03 import guided_modes_1DTE
+
+
+plt.close('all')
+
+grid_size     = ?? # please choose appropriate value
+number_points = ?? # please choose appropriate value
+h             = ?? # please choose appropriate value
+lam           = 0.78
+k0            = 2*np.pi/lam
+e_substrate   = 1.5**2
+delta_e       = 1.5e-2
+w             = 15.0
