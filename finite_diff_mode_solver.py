0ace084c53f5e0eabfa0e6435d2f7e6e855f8252
roflmaostc <f.wechsler@tum.de>
2020-05-15 19:56:56 +0200

    Working code for Ex1

--- finite_diff_mode_solver.py
+++ finite_diff_mode_solver.py
@@ -28,25 +28,37 @@ def guided_modes_1DTE(prm, k0, h):
         Field distributions of the guided eigenmodes
     """
     out_shape = (len(prm), len(prm))
-    # create empty result arrays
-    eff_eps = []
-    guided = np.zeros(out_shape)
 
     # diagonal of sparse matrix
-    den = 1 / k0 ** 2 / h **2
+    den = 1 / k0 ** 2 / h ** 2
     diag = -2 * den + prm
-    # neighbour of the diagonal matrix
+    # of diagonals of the matrix
     diag_above = np.ones(prm.shape) * 1 * den
-    diag_below = diag_above 
+    diag_below = diag_above
 
     # fill the sparse matrix with the data
     data = np.array([diag, diag_above, diag_below])
     offset = np.array([0, 1, -1])
     L = sps.dia_matrix((data, offset), shape=out_shape)
-                        
-    return L.todense()
-    # return guided, eff_eps
 
+    # calculate eigenvalues and take real part of them
+    # absorption is negligibe
+    ev, vecs = eigs(L, k=len(prm) - 2)
+    ev = np.real(ev)
+    vecs = np.real(vecs)
+    # swap axis in vecs, because vecs are aligned horizontally
+    vecs = vecs.swapaxes(1, 0)
+
+    # filter for the physical interesting
+    # first we create a binary mask which checks for the conditions
+    mask = np.logical_and(ev > prm[0], ev < np.max(prm))
+
+    # extract these eigenvalues
+    eff_eps = ev[mask]
+    # also extract the eigenvectors
+    vecs = vecs[mask]
+
+    return eff_eps, vecs
 
 def guided_modes_2D(prm, k0, h, numb):
     """Computes the effective permittivity of a quasi-TE polarized guided 
