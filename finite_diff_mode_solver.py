b9396a821f853eae7cb9dfdf4eb8d633e91d3551
roflmaostc <f.wechsler@tum.de>
2020-05-15 16:20:49 +0200

    Add Matrix creation of L to code

--- finite_diff_mode_solver.py
+++ finite_diff_mode_solver.py
@@ -10,7 +10,7 @@ def guided_modes_1DTE(prm, k0, h):
     All dimensions are in µm.
     Note that modes are filtered to match the requirement that
     their effective permittivity is larger than the substrate (cladding).
-    
+
     Parameters
     ----------
     prm : 1d-array
@@ -19,7 +19,7 @@ def guided_modes_1DTE(prm, k0, h):
         Free space wavenumber
     h : float
         Spatial discretization
-    
+
     Returns
     -------
     eff_eps : 1d-array
@@ -27,7 +27,25 @@ def guided_modes_1DTE(prm, k0, h):
     guided : 2d-array
         Field distributions of the guided eigenmodes
     """
-    pass
+    out_shape = (len(prm), len(prm))
+    # create empty result arrays
+    eff_eps = []
+    guided = np.zeros(out_shape)
+
+    # diagonal of sparse matrix
+    den = 1 / k0 ** 2 / h **2
+    diag = -2 * den + prm
+    # neighbour of the diagonal matrix
+    diag_above = np.ones(prm.shape) * 1 * den
+    diag_below = diag_above 
+
+    # fill the sparse matrix with the data
+    data = np.array([diag, diag_above, diag_below])
+    offset = np.array([0, 1, -1])
+    L = sps.dia_matrix((data, offset), shape=out_shape)
+                        
+    return L.todense()
+    # return guided, eff_eps
 
 
 def guided_modes_2D(prm, k0, h, numb):
